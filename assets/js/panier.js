0b20188b91db378a4cf52363b71923c14dee1958
Tom <tom.zidani@gmail.com>
2020-05-03 18:43:19 +0200

    Amélioration du JS

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -14,86 +14,72 @@ var validerBtn = document.getElementById("valider");
 // Affichage du panier
 function affichagePanier(data) {
 
-	// Si on a un article dans le panier
-	if(panier != null){
-		if(panier.length > 0){
-		
-			// Boucle pour chaque article du panier
-			for(let i = 0; i < panier.length; i++) {
+    // Si on a un article dans le panier
+    if (panier != null && panier.length > 0) {
 
+        // Boucle pour chaque article du panier
+        for (let i = 0; i < panier.length; i++) {
 
+            const produitDesDonnees = data.find(element => element._id === panier[i]._id);
 
-				// Boucle pour chaque produit
-				for(let t = 0; t < data.length; t++) {
+            if (produitDesDonnees) {
 
-					// Si l'id du produit correspond à l'id de l'article du panier
-					if(panier[i]._id == data[t]._id){
+                // Création du HTML
+                let listePanier = '<div class="produit-panier">';
+                listePanier += '<a href="./produit.html?id=' + produitDesDonnees._id + '"><img src="' + produitDesDonnees.imageUrl + '" class="produit-image"></a>';
+                listePanier += '<div class="produit-informations">';
+                listePanier += '<h3><a href="./produit.html?id=' + produitDesDonnees._id + '">' + produitDesDonnees.name + '</a></h3>';
+                listePanier += '<p>' + produitDesDonnees.price / 100 + '&euro;</p>';
+                listePanier += '<div class="btn" onclick="supprimerProduit(\'' + produitDesDonnees._id + '\')">Retirer l\'article</div>';
+                listePanier += '</div></div>';
+                // Insertion du HTML
+                panierDiv.innerHTML += listePanier
 
-						// Création du HTML
-						let listePanier = '<div class="produit-panier">';
-						listePanier += '<a href="./produit.html?id='+ data[t]._id +'"><img src="'+ data[t].imageUrl +'" class="produit-image"></a>';
-						listePanier += '<div class="produit-informations">';
-						listePanier += '<h3><a href="./produit.html?id='+ data[t]._id +'">'+ data[t].name +'</a></h3>';
-						listePanier += '<p>'+ data[t].price/100 +'&euro;</p>';
-						listePanier += '<div class="btn" onclick="supprimerProduit(\''+ data[t]._id +'\')">Retirer l\'article</div>';
-						listePanier += '</div></div>';
-						// Insertion du HTML
-						panierDiv.innerHTML += listePanier
+                // Augmentation du prix total
+                prixTotal += produitDesDonnees.price;
+            }
 
-						// Augmentation du prix total
-						prixTotal += data[t].price;
-					}
-				}
-			}
+        }
 
-		// Affichage du panier
-		prixTotalDiv.innerHTML = "Total: "+ prixTotal/100 +"&euro;";
-	}
+        // Affichage du panier
+        prixTotalDiv.innerHTML = "Total: " + prixTotal / 100 + "&euro;";
 
-	// Sinon, si le panier est vide, on affiche un message et on retire le bouton
-	else{
+    }
 
-		let messageVide = '<p class="centre">Votre panier est tristement vide...</p>';	
-		panierDiv.innerHTML += messageVide;
-		validerBtn.style.display = "none";
+    // Sinon, si le panier est vide, on affiche un message et on retire le bouton
+    else {
 
-	}
+        let messageVide = '<p class="centre">Votre panier est tristement vide...</p>';
+        panierDiv.innerHTML += messageVide;
+        validerBtn.style.display = "none";
 
-	}
-	// Sinon, si le panier est vide, on affiche un message et on retire le bouton
-	else{
-
-		let messageVide = '<p class="centre">Votre panier est tristement vide...</p>';	
-		panierDiv.innerHTML += messageVide;
-		validerBtn.style.display = "none";
-
-	}
+    }
 }
 
 // Création de la fonction permettant de supprimer un produit du panier
-function supprimerProduit(id){
+function supprimerProduit(id) {
 
-	// Boucle vérifiant quel produit veut-on supprimer
-	for(let i = 0; i < panier.length; i++) {
+    // Boucle vérifiant quel produit veut-on supprimer
+    for (let i = 0; i < panier.length; i++) {
 
-		// Si l'id du produit correspond à l'article du panier on stocke son index
-		if(panier[i].id == id) {
-			var indexSuppression = i;
-		}
-	}
+        // Si l'id du produit correspond à l'article du panier on stocke son index
+        if (panier[i].id == id) {
+            var indexSuppression = i;
+        }
+    }
 
-	// On récupère le panier et on lui retire l'article sélectionné à l'aide de l'index récuperé plus tôt
-	panier.splice(indexSuppression, 1);
+    // On récupère le panier et on lui retire l'article sélectionné à l'aide de l'index récuperé plus tôt
+    panier.splice(indexSuppression, 1);
 
-	// Puis on réinsère les informations dans le panier
-	localStorage.setItem("panier", [JSON.stringify(panier)]);
-	location.reload();
+    // Puis on réinsère les informations dans le panier
+    localStorage.setItem("panier", [JSON.stringify(panier)]);
+    location.reload();
 
-	// Et on notifie l'utilisateur
-	modal("Notification", "Cet article a été supprimé de votre panier");
+    // Et on notifie l'utilisateur
+    modal("Notification", "Cet article a été supprimé de votre panier");
 }
 
 // Ajout des évènements
 validerBtn.addEventListener("click", function() {
-	modal("formulaire");
+    modal("formulaire");
 })
--- assets/js/produit.js
+++ assets/js/produit.js
@@ -1,95 +1,83 @@
 // Récupération de l'URL
-let url = window.location.href;
-url = url.split('?id=');
-const id = url[1];
+const url = window.location.href.split("?id=")[1];
 
 // Si aucun produit n'est défini, on retourne a l'accueil
-if(!id){
-	document.location.href = "index.html";
+if (!url) {
+    document.location.href = "index.html";
 }
 
 // Connexion à l'API
-connexionAPI(afficherProduit, "http://localhost:3000/api/cameras/"+ id);
+connexionAPI(afficherProduit, "http://localhost:3000/api/cameras/" + url);
 
 // Déclaration des variables
 
-	// Produits
-	let photoProduit = document.getElementById("photo");
-	let prixProduit = document.getElementById("prix");
-	let nomProduit = document.querySelector("h2");
-	let lentillesProduit = document.getElementById("lentilles");
-	let descProduit = document.getElementById("desc");
-	let champProduit = document.querySelector(".produit-page");
+// Produits
+let photoProduit = document.getElementById("photo");
+let prixProduit = document.getElementById("prix");
+let nomProduit = document.querySelector("h2");
+let lentillesProduit = document.getElementById("lentilles");
+let descProduit = document.getElementById("desc");
+let champProduit = document.querySelector(".produit-page");
 
-	// Boutons & inputs
-	let ajouterAuPanier = document.getElementById("panier");
+// Boutons & inputs
+let ajouterAuPanier = document.getElementById("panier");
 
 // Affichage des données
-function afficherProduit(data){
+function afficherProduit(data) {
 
-	// Récupération du produit
-	const produit = data;
+    // Récupération du produit
+    const produit = data;
+    console.log(data);
 
-	// Si il n'y a pas de produit correspondant à l'ID récupéré
-	if(!produit) {
+    // Si il n'y a pas de produit correspondant à l'ID récupéré
+    if (produit.name === 'CastError' || produit === '{ }') {
 
-		// Création d'un message d'erreur
-		champProduit.style.display = "block";
-		let messageErreur = '<h2 class="centre">Oups, il semblerait que ce produit n\'existe pas..</h2><br>';
-		messageErreur += '<p class="centre">Tu peux revenir à l\'accueil en cliquant <a href="./index.html"><span>ICI</span></a></p>';
+        // Création d'un message d'erreur
+        champProduit.style.display = "block";
+        let messageErreur = '<h2 class="centre">Oups, il semblerait que ce produit n\'existe pas..</h2><br>';
+        messageErreur += '<p class="centre">Tu peux revenir à l\'accueil en cliquant <a href="./index.html"><span>ICI</span></a></p>';
 
-		// Insertion du message d'erreur
-		champProduit.innerHTML = messageErreur;
-	}
+        // Insertion du message d'erreur
+        champProduit.innerHTML = messageErreur;
+    }
 
-	// Sinon
-	else {
+    // Sinon
+    else {
 
-		// Déclaration de la variable lentilles
-		let lentilles = "";
+        // Déclaration de la variable lentilles
+        const lentilles = produit.lenses.join(", ");
 
-		// Création d'une boucle pour récupérer les lentilles
-		for(let i = 0; i < produit.lenses.length; i++){
-
-			// Si la variable lentille est vide, alors on y stocke la première lentille
-			if(lentilles == "") {
-				lentilles = produit.lenses[i];
-			}
-			// Sinon, on y ajoute la deuxième lentille avec une virgule
-			else {
-				lentilles += ", "+ produit.lenses[i];
-			}
-		}
-
-		// Insertion des informations du produit dans la page
-		nomProduit.textContent = produit.name;
-		lentillesProduit.textContent = lentilles;
-		descProduit.textContent = produit.description;
-		prixProduit.innerHTML = produit.price/100 + "&euro;";
-		photoProduit.setAttribute("src", produit.imageUrl);
-	}
+        // Insertion des informations du produit dans la page
+        nomProduit.textContent = produit.name;
+        lentillesProduit.textContent = lentilles;
+        descProduit.textContent = produit.description;
+        prixProduit.innerHTML = produit.price / 100 + "&euro;";
+        photoProduit.setAttribute("src", produit.imageUrl);
+    }
 }
 
 // Création d'une fonction pour l'ajout au panier
 function ajouterPanier() {
 
-	// Récupération des informations du panier
-	let panier = JSON.parse(localStorage.getItem("panier")) || [];
+    // Récupération des informations du panier
+    let panier = JSON.parse(localStorage.getItem("panier")) || [];
 
-	// La variable contenant les informations
-	var informationsObjet = {"_id": id};
+    // La variable contenant les informations
+    var informationsObjet = {
+        "_id": url
+    };
 
-	// Si le panier est vide on stocke simplement les informations du produit dedans
-	if(!panier) {
-		localStorage.setItem("panier", [JSON.stringify(informationsObjet)]);
-	}
+    // Si le panier est vide on stocke simplement les informations du produit dedans
+    if (!panier) {
+        localStorage.setItem("panier", [JSON.stringify(informationsObjet)]);
+    }
 
-	// Sinon on ajoute les informations du produit à celles déjà stockées
-	else {
-		panier.push(informationsObjet);
-		localStorage.setItem("panier", [JSON.stringify(panier)]);
-	}
-	modal("notification", "Article ajouté au panier");
+    // Sinon on ajoute les informations du produit à celles déjà stockées
+    else {
+        panier.push(informationsObjet);
+        localStorage.setItem("panier", [JSON.stringify(panier)]);
+    }
+    modal("notification", "Article ajouté au panier");
 }
 
 // Ajout des évènements
