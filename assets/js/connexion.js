4a3a49a1dad0b75dffeca4f9ccaead796c7614e9
Tom <tom.zidani@gmail.com>
2020-05-06 21:16:08 +0200

    Changement de la fonction de connexion

--- assets/js/connexion.js
+++ assets/js/connexion.js
@@ -4,15 +4,18 @@ var API = "http://localhost:3000/api/cameras";
 // Création d'une fonction de connexion à l'API
 function connexionAPI(fonctionRetour, lien) {
 	if(lien){ API = lien; }
-	fetch(API)
-	  .then(function (response) {
-	    return response.json();
-	  })
-	  .then(function (data) {
-	    fonctionRetour(data);
-	    console.log("Connexion à l'API effectuée.");
-	  })
-	  .catch(function (err) {
-	    console.log(err);
-	  });	
+	
+	return new Promise ((resolve, reject) => {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+		    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+		    	const data = JSON.parse(this.responseText);
+		        fonctionRetour(data);
+		    }
+		};
+		request.open("GET", API);
+		request.send();
+	});
+
+
 }
